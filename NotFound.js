733c64088ee718eb0e7a281e24e7b49fd052da45
princeflexzy0 <emmanuelatere44@gmail.com>
2025-10-02 05:23:34 +0100

    Create client/src/pagesNotFound.js

---
+++ NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h2>404 - Page Not Found</h2>
+      <p>The requested page does not exist.</p>
+    </div>
+  );
+}
+
+export default NotFound;
